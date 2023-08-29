4d7b8dc58416523fa8ff5bd9f9c327983e52705d
elgharbi <elgharbiay@gmail.com>
2023-08-29 23:40:00 +0100

    done

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_is_root - check if node is the root
+  * @node: node to check
+  * Return: 1 if node is the root otherwise 0
+  */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node || node->parent)
+		return (0);
+	return (1);
+}
