52dac9a48f3d242bed68046249bd6697d0507466
elgharbi <elgharbiay@gmail.com>
2023-08-29 17:42:31 +0100

    done

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes a binary tree
+ * @tree: binary tree
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
