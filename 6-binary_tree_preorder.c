73dd35d1b66dac43ac0952cb487472a142eb1201
elgharbi <elgharbiay@gmail.com>
2023-08-30 00:31:23 +0100

    done

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - does function using preorder
+ * @tree: binary tree
+ * @func: function to do in preorder
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
