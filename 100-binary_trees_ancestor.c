6c86cb14ba462e07afbe68cc79a4e149a6331d1a
elgharbi <elgharbiay@gmail.com>
2023-08-31 22:39:32 +0100

    done

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,13 +6,14 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
     if (!first || !second)
         return (NULL);
 
-    if (first->parent != second->parent || first != second->parent)
-        if (first != second->parent && second ->)
-            binary_trees_ancestor(first, second->parent);
-        else
-            return (second->parent);
-        if 
+    if (first == second)
+        return (binary_tree_t *)(first);
 
+    if (first == second->parent || (!first->parent && second->parent))
+        return (binary_tree_t *)(first);
 
-    return (second->parent);
+    if (second == first->parent || (!second->parent && first->parent))
+        return (binary_tree_t *)(second);
+
+    return binary_trees_ancestor(first->parent, second->parent);
 }
